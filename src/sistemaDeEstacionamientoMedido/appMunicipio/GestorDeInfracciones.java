60350bfbf151d238e9bd4f8bf829f0e4fe769c9a
Julian D. Trejo <trejojulian998@gmail.com>
2020-11-06 20:59:21 -0300

    Se añadió la clase GestorDeInfracciones con su respectivo test.

---
+++ src/sistemaDeEstacionamientoMedido/appMunicipio/GestorDeInfracciones.java
@@ -0,0 +1,25 @@
+package sistemaDeEstacionamientoMedido.appMunicipio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestorDeInfracciones implements IGestorDeInfracciones {
+	private List <Infraccion> infracciones;
+	
+	public GestorDeInfracciones () {
+		this.infracciones = new ArrayList <Infraccion>();
+	}
+
+	@Override
+	public void registrarInfraccion(Infraccion infraccion) {
+		this.infracciones.add(infraccion);
+		
+	}
+
+	@Override
+	public List<Infraccion> getInfracciones() {
+
+		return this.infracciones;
+	}
+
+}
