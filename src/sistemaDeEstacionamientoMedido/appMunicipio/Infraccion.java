13808e1cdb97d7a3d712d5574b2d28a072a62913
Julian D. Trejo <trejojulian998@gmail.com>
2020-11-06 20:26:15 -0300

    Se añadieron las infracciones con su respectivo test

--- src/sistemaDeEstacionamientoMedido/appMunicipio/Infraccion.java
+++ src/sistemaDeEstacionamientoMedido/appMunicipio/Infraccion.java
@@ -1,5 +1,34 @@
 package sistemaDeEstacionamientoMedido.appMunicipio;
 
+import java.time.LocalDateTime;
+
 public class Infraccion {
+	private String patente;
+	private LocalDateTime fechaYHora;
+	private Inspector inspector;
+	private Zona zona;
+
+	public Infraccion(String patente, LocalDateTime fechaYHora, Inspector inspector, Zona zona) {
+		this.patente = patente;
+		this.fechaYHora = fechaYHora;
+		this.inspector = inspector;
+		this.zona = zona;
+	}
+
+	public String getPatente() {
+		return patente;
+	}
+
+	public LocalDateTime getFechaYHora() {
+		return fechaYHora;
+	}
+
+	public Inspector getInspector() {
+		return inspector;
+	}
+
+	public Zona getZona() {
+		return zona;
+	}
 
 }
