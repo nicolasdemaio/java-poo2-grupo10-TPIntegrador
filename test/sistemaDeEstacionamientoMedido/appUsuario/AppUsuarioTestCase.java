69f54b7f6db9e77e5eb061aaa5be73c5e69b2110
Nicolas De Maio <nicolas.demaio19@gmail.com>
2020-11-11 20:23:14 -0300

    Se finalizaron los test de la appUsuario

--- test/sistemaDeEstacionamientoMedido/appUsuario/AppUsuarioTestCase.java
+++ test/sistemaDeEstacionamientoMedido/appUsuario/AppUsuarioTestCase.java
@@ -5,10 +5,10 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import sistemaDeEstacionamientoMedido.appMunicipio.IGestorDeEstacionamientos;
 import sistemaDeEstacionamientoMedido.appMunicipio.Infraccion;
 import sistemaDeEstacionamientoMedido.estacionamiento.Estacionamiento;
 import sistemaDeEstacionamientoMedido.estacionamiento.EstacionamientoMedianteApp;
+import sistemaDeEstacionamientoMedido.gestorDeEstacionamiento.IGestorDeEstacionamientos;
 import sistemaDeEstacionamientoMedido.gestorDeSaldo.IGestorDeSaldo;
 import sistemaDeEstacionamientoMedido.notificacion.Notificacion;
 import sistemaDeEstacionamientoMedido.sem.ServicioAppUsuario;
@@ -204,6 +204,173 @@ class AppUsuarioTestCase {
 		verify(gestorDeEstacionamientos, never()).añadirEstacionamiento(any(EstacionamientoMedianteApp.class), any(Integer.class));
 		verify(gestorDeSuscriptores, never()).notifySuscriptores(any(Notificacion.class));
 	}
+	
+
+	@Test
+	void testCuandoNoEsHorarioDeEstacionamientoAlIniciarEstacionamiento_SeRecibeUnaRespuestaQueNoCorrespondeEstacionar() {
+		//SETUP
+		
+		//El punto  pertenece a la zona.
+		Point2D unPunto = mock(Point2D.class);
+		when (celular.getPosicion()).thenReturn(unPunto);
+		when (gestorDeZonas.perteneceAUnaZona(unPunto)).thenReturn(true);
+		//No Hay estacionamiento Vigente
+		when (gestorDeEstacionamientos.hayEstacionamientoVigente(patente)).thenReturn(false);
+		//No es Horario de estacionamiento
+		LocalDateTime horaActual = LocalDateTime.now();
+		when (celular.getFechaYHoraActual()).thenReturn(horaActual);
+		when (gestorDeEstacionamientos.esHorarioDeEstacionamiento(horaActual)).thenReturn(false);
+		
+		//EXERCISE
+		
+		String respuesta = app.iniciarEstacionamiento().toString();
+		String respuestaEsperada = "No corresponde iniciar el estacionamiento";
+		
+		//VERIFY
+		
+		assertEquals(respuestaEsperada, respuesta);
+		assertEquals(ubicacionActual, app.getUltimoPuntoDeEstacionamiento());
+		verify(gestorDeEstacionamientos, never()).añadirEstacionamiento(any(EstacionamientoMedianteApp.class), any(Integer.class));
+		verify(gestorDeSuscriptores, never()).notifySuscriptores(any(Notificacion.class));
+	}
+	
+	@Test
+	void testCuandoNoHaySaldoSuficienteAlIniciarEstacionamiento_SeRecibeUnaRespuestaQueNoHaySaldoSuficiente() {
+		//SETUP
+		
+		//El punto  pertenece a la zona.
+		Point2D unPunto = mock(Point2D.class);
+		when (celular.getPosicion()).thenReturn(unPunto);
+		when (gestorDeZonas.perteneceAUnaZona(unPunto)).thenReturn(true);
+		//No Hay estacionamiento Vigente
+		when (gestorDeEstacionamientos.hayEstacionamientoVigente(patente)).thenReturn(false);
+		//Es Horario de estacionamiento
+		LocalDateTime horaActual = LocalDateTime.now();
+		when (celular.getFechaYHoraActual()).thenReturn(horaActual);
+		when (gestorDeEstacionamientos.esHorarioDeEstacionamiento(horaActual)).thenReturn(true);
+		//No hay Saldo
+		Integer nroCelu = 42214560;
+		Double mockSaldo = 25d;
+		when (celular.getNumeroDeCelular()).thenReturn(nroCelu);
+		when (gestorDeSaldo.getSaldo(nroCelu)).thenReturn(mockSaldo);
+		when (gestorDeEstacionamientos.esSaldoSuficiente(mockSaldo)).thenReturn(false);
+		
+		//EXERCISE
+	
+		String respuesta = app.iniciarEstacionamiento().toString();
+		String respuestaEsperada = "Saldo insuficiente. Estacionamiento no permitido.";
+		
+		//VERIFY
+		
+		assertEquals(respuestaEsperada, respuesta);
+		assertEquals(ubicacionActual, app.getUltimoPuntoDeEstacionamiento());
+		verify(gestorDeEstacionamientos, never()).añadirEstacionamiento(any(EstacionamientoMedianteApp.class), any(Integer.class));
+		verify(gestorDeSuscriptores, never()).notifySuscriptores(any(Notificacion.class));
+	}
+	
+	@Test
+	void testIniciarEstacionamientoCuandoEstanDadasLasCondiciones() {
+		//SETUP
+		
+		//El punto  pertenece a la zona.
+		Point2D unPunto = mock(Point2D.class);
+		when (celular.getPosicion()).thenReturn(unPunto);
+		when (gestorDeZonas.perteneceAUnaZona(unPunto)).thenReturn(true);
+		//No Hay estacionamiento Vigente
+		when (gestorDeEstacionamientos.hayEstacionamientoVigente(patente)).thenReturn(false);
+		//Es Horario de estacionamiento
+		LocalDateTime horaActual = LocalDateTime.now();
+		when (celular.getFechaYHoraActual()).thenReturn(horaActual);
+		when (gestorDeEstacionamientos.esHorarioDeEstacionamiento(horaActual)).thenReturn(true);
+		//Hay Saldo
+		Integer nroCelu = 42214560;
+		Double mockSaldo = 25d;
+		when (celular.getNumeroDeCelular()).thenReturn(nroCelu);
+		when (gestorDeSaldo.getSaldo(nroCelu)).thenReturn(mockSaldo);
+		when (gestorDeEstacionamientos.esSaldoSuficiente(mockSaldo)).thenReturn(true);
+		//Se mockea la hora maxima
+		LocalDateTime horaMaxima = LocalDateTime.now().plusHours(3);
+		when (gestorDeEstacionamientos.horaMaximaDeFin(mockSaldo)).thenReturn(horaMaxima);
+		
+		//EXERCISE
+		
+		String respuesta = app.iniciarEstacionamiento().toString();
+		String respuestaEsperada = "Hora de Inicio: " + horaActual.getHour() +
+				                   "\nHora Maxima: " + horaMaxima.getHour();
+		//VERIFY
+		
+		assertEquals(respuestaEsperada, respuesta);
+		assertEquals(unPunto, app.getUltimoPuntoDeEstacionamiento());
+		verify(gestorDeEstacionamientos, times(1)).añadirEstacionamiento(any(EstacionamientoMedianteApp.class), any(Integer.class));
+		verify(gestorDeSuscriptores, times(1)).notifySuscriptores(any(Notificacion.class));
+	}
+	
+	@Test
+	void testCuandoNoSeEstaEnLaUltimaPosicionDeEstacionamientoYSeFinalizaEstacionamiento_SeRecibeUnaRespuestaQueNoCorrespondeFinalizar() {
+		//Se cambia la posicion actual
+		Point2D otroPunto = mock(Point2D.class);
+		when(celular.getPosicion()).thenReturn(otroPunto);
+		
+		String respuesta = app.finDeEstacionamiento().toString();
+		String respuestaEsperada = "No corresponde finalizar el estacionamiento";
+		
+		assertEquals (respuesta, respuestaEsperada);
+		verify (gestorDeEstacionamientos, never()).finalizarVigenciaDeEstacionamiento(any(Integer.class));
+		verify (gestorDeSaldo, never()).disminuirSaldo(any(Integer.class), any(Double.class));
+		verify (gestorDeSuscriptores, never()).notifySuscriptores(any(Notificacion.class));
+		
+	}
+	
+	@Test
+	void testCuandoNoHayEstacionamientoVigenteYSeFinalizaEstacionamiento_SeRecibeUnaRespuestaQueNoCorrespondeFinalizar() {
+		//No hay estacionamiento Vigente.
+		when(gestorDeEstacionamientos.hayEstacionamientoVigente(patente)).thenReturn(false);
+		
+		String respuesta = app.finDeEstacionamiento().toString();
+		String respuestaEsperada = "No corresponde finalizar el estacionamiento";
+	
+		
+		assertEquals (respuesta, respuestaEsperada);
+		verify (gestorDeEstacionamientos, times(0)).finalizarVigenciaDeEstacionamiento(any(Integer.class));
+		verify (gestorDeSaldo, times(0)).disminuirSaldo(any(Integer.class), any(Double.class));
+		verify (gestorDeSuscriptores, times(0)).notifySuscriptores(any(Notificacion.class));
+	}
+	
+	@Test
+	void testFinDeEstacionamientoCuandoEstanDadasLasCondiciones() {
+		//SETUP
+		//Esta en el mismo punto, y hay estacionamiento vigente
+		when(gestorDeEstacionamientos.hayEstacionamientoVigente(patente)).thenReturn(true);
+
+		Integer nroCelular = 42243021;
+		when(celular.getNumeroDeCelular()).thenReturn(nroCelular);
+		//Se mockea el estacionamiento finalizado
+		Estacionamiento estacionamiento = mock(Estacionamiento.class);
+		LocalDateTime horaDeInicio = LocalDateTime.now();
+		LocalDateTime horaFin = horaDeInicio.plusHours(2);
+		Double costoEstacionamiento = 80d;
+		when(estacionamiento.getCosto()).thenReturn(costoEstacionamiento);
+		when(estacionamiento.getHoraDeInicio()).thenReturn(horaDeInicio);
+		when(estacionamiento.getHoraDeInicio()).thenReturn(horaFin);
+		when(gestorDeEstacionamientos.finalizarVigenciaDeEstacionamiento(nroCelular)).thenReturn(estacionamiento);
+
+		//EXERCISE
+		
+		String respuesta = app.finDeEstacionamiento().toString();
+		String respuestaEsperada = "Hora de Inicio: " + horaDeInicio.getHour() +
+		                           "\nHora de Fin: " + horaFin.getHour() +
+		                           "\nDuracion (horas): " + 2 +
+		                           "\nCosto: " + costoEstacionamiento;
+				
+		//VERIFY 
+		
+		verify (gestorDeEstacionamientos, times(1)).finalizarVigenciaDeEstacionamiento(nroCelular);
+		verify (gestorDeSaldo, times(1)).disminuirSaldo(nroCelular, costoEstacionamiento);
+		verify (gestorDeSuscriptores, times(1)).notifySuscriptores(any(Notificacion.class));
+		assertEquals(respuestaEsperada,respuesta);
+		
+	}
+
 	
 
 }
