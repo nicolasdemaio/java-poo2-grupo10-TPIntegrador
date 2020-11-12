ec08c9be6d7e61816cc98c88d3c824f7cbbf3bc7
Julian D. Trejo <trejojulian998@gmail.com>
2020-11-12 16:20:39 -0300

    Se añadieron Javadocs y se cambio el nombre del colaborador de disminuir saldo

--- src/sistemaDeEstacionamientoMedido/gestorDeSaldo/GestorDeSaldo.java
+++ src/sistemaDeEstacionamientoMedido/gestorDeSaldo/GestorDeSaldo.java
@@ -3,11 +3,12 @@ package sistemaDeEstacionamientoMedido.gestorDeSaldo;
 import java.util.HashMap;
 import java.util.Map;
 
-/*
+/**
  * Inveriante de representación: El nro del celular consiste de un Integer de 8 digitos, y 
  *                               no puede existir un numero de celular que corresponda a 2
  *                               celulares distintos.
  */
+
 public class GestorDeSaldo {
 	private Map <Integer, Double> saldoPorCelular;
 	
@@ -20,7 +21,8 @@ public class GestorDeSaldo {
 		return this.saldoPorCelular;
 	}
 	
-	/*Actualiza el saldo asignado al celular sumandole la cantidad indicada por parametro.
+	/**
+	 * Actualiza el saldo asignado al celular sumandole la cantidad indicada por parametro.
 	 * Si es la primera vez que se carga saldo, se hara una referencia al celular con la
 	 * cantidad indicada. 
 	 */
@@ -28,23 +30,26 @@ public class GestorDeSaldo {
 		this.getSaldoPorCelular().put(nroCelular, this.getSaldo(nroCelular) + saldo);
 	}
 	
-	/*Describe el saldo que posee el celular indicado. En caso de no estar registrado,
+	/**
+	 * Describe el saldo que posee el celular indicado. En caso de no estar registrado,
 	 * indica que posee saldo 0.
+	 * @return Double 
 	 */
 	public Double getSaldo(Integer nroCelular) {
 		
 		return this.getSaldoPorCelular().getOrDefault(nroCelular,0d);
 	}
 	
-	/*
+	/**
 	 * Se disminuye la cantidad de saldo indicada por parametro al nro de celular indicado.
 	 * Si la cantidad a disminuir excede al saldo, este queda en 0.
 	 * Si el nro de celular no se encuentra registrado en el sistema, se levantará una excepción.
+	 * @exception ActualizacionDeSaldoException
 	 */
-	public void disminuirSaldo(Integer nroCelular, Double saldo) {
+	public void disminuirSaldo(Integer nroCelular, Double saldoARestar) {
 		try {
 			this.getSaldoPorCelular().put(nroCelular,
-					                      Math.max(this.getSaldoPorCelular().get(nroCelular) - saldo,
+					                      Math.max(this.getSaldoPorCelular().get(nroCelular) - saldoARestar,
 					                    		   0));	
 		} catch (NullPointerException e){
 			throw new ActualizacionDeSaldoException ("El celular no se encuentra en el sistema", e);
